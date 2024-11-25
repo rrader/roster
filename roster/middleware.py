6f6602edcd3436d8041a49fc9153e548e7e89cbd
Roman Rader <roman.rader@grammarly.com>
2024-11-25 15:17:32 +0200

    fix

---
+++ roster/middleware.py
@@ -0,0 +1,13 @@
+
+
+class MoodleLogoutMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        response.delete_cookie('MoodleSession')
+
+        return response
+
