a8ddcb4a60aec4ec369e2bc27ac1cdfebcba7df0
Roman Rader <roman.rader@grammarly.com>
2024-11-25 15:34:39 +0200

    fix

--- moodleroster/settings.py
+++ moodleroster/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f@ma$_^0@^7)edrprt@ig-qra+()0en7@qxelp)yox*eskb&m8
 DEBUG = True
 
 BASE_DOMAIN = 'rmn.pp.ua'
-ALLOWED_HOSTS = [f'roster.{BASE_DOMAIN}', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [f'roster.{BASE_DOMAIN}', '127.0.0.1', 'localhost', 'BASE_DOMAIN', 'sites.google.com']
 CSRF_TRUSTED_ORIGINS = [f'https://roster.{BASE_DOMAIN}']
 
 
